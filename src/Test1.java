1b471616e41fcbe950c7eda2a4b89e73013953b2
v-vijay.prasad <v-vijay.prasad@MDL-211-1QCF9X2.magicsw.com>
2020-12-22 13:23:53 +0530

    First commit...

---
+++ src/Test1.java
@@ -0,0 +1,14 @@
+
+public class Test1 {
+
+	public static void main(String[] args)
+	{
+	  String hungry = "yes";
+	  if(hungry == "yes")
+		  System.out.println("Eat something...");
+	  else {
+		  System.out.println("Do work...");
+	  }
+	}
+
+}
